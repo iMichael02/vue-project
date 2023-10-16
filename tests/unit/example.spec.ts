b3f09fb8eb625eb44ef904332d427cdd531702bd
iMichael02 <btm.qtv@gmail.com>
2023-10-16 14:57:34 +0700

    add test for AuthOptions, HomeView, LoginForm

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -3,7 +3,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
+    const msg = "Welcome to Vuetify 3 Beta  For help and collaboration with other Vuetify developers, please join our online Discord CommunityWhat's next?Explore componentsRoadmapFrequently Asked QuestionsImportant LinksChatMade with VuetifyTwitterArticlesEcosystemvuetify-loadergithubawesome-vuetify";
     const wrapper = shallowMount(HelloWorld, {
       props: { msg },
     });
